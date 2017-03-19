f1511d65569e6614bf97eab21d44ea2bac55810a
Jarrad Tom <contact@tomja.com>
2017-03-19 13:18:12 +1100

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # myportfolio
-my personal portfolio website project
+My personal portfolio website project.
+
+## Installation
+You first need to download and install Node.js, a JavaScript engine that runs
+natively on your OS. Node.js includes a command-line JavaScript package manager
+called `npm`.
+
+Download and install the latest Node.js build - https://docs.npmjs.com/getting-started/installing-node
+
+Then, clone this repository into a folder in your computer, enter it in the command line
+and simply run `npm install`. This will download all the files necessary for the build process
+and all JavaScript files that will be bundled into the actual build (which is what you actually serve to end users on the web).
+
+## Building
+There is a `package.json` file in the root directory of the project, which is a file
+that has a variety of configuration options and a list of dependencies for your project.
+
+You will notice that there is a `scripts` config object containing two scripts - `dev` and `build`.
+You can run these scripts by doing `npm run *script-name*` in your terminal, where
+`*script-name*` is one of these scripts. Eg - `npm run build`.
+
+`npm run build` will bundle all of your HTML, JS, CSS and SCSS files in the `src/` directory
+and put the bundled files in the `build/` directory.
+
+`npm run dev` will bundle all of your files and also open local server on your PC
+that you can use for development purposes. The server will auto-refresh when it detects a
+change in any files it's watching in the `src/` directory.
+
+## Deployment
+To get the built site up on the internet, simply run `npm run build` to build the project.
+The contents of the `build/` folder after running the build command are what you actually
+want up on your server. So simply build, then upload the contents of `build/` to your server
+via FTP.
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    context: __dirname + '/',
+    entry: {
+        app: './src/index.js',
+        vendor: ['angular']
+    },
+    output: {
+        path: __dirname + '/build',
+        filename: 'app.bundle.js'
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin( { name: "vendor", filename: "vendor.bundle.js" }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: false
+        }),
+        new ExtractTextPlugin({
+            filename: 'bundle.css'
+        })
+    ],
+    devServer: {
+        host: '0.0.0.0',
+        historyApiFallback: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: [
+                    path.resolve(__dirname, 'src')
+                ],
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.html$/,
+                include: [
+                    path.resolve(__dirname, 'src')
+                ],
+                loader: 'html-loader'
+            },
+            {
+                test: /\.scss$/,
+                include: [
+                    path.resolve(__dirname, 'src')
+                ],
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader!autoprefixer-loader!sass-loader'
+                })
+            },
+            {
+                test: /\.css$/,
+                include: [
+                    path.resolve(__dirname, 'node_modules/angular-material/'),
+                    path.resolve(__dirname, 'node_modules/material-design-icons/iconfont')
+                ],
+                loader: 'style-loader!css-loader!autoprefixer-loader'
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                include: [
+                    path.resolve(__dirname, 'node_modules/material-design-icons/iconfont')
+                ],
+                loader: "url-loader?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.(ttf|eot|svg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                include: [
+                    path.resolve(__dirname, 'node_modules/material-design-icons/iconfont')
+                ],
+                loader: "file-loader"
+            }
+        ]
+    },
+};
